133cd6a5c2d299cdabe986343d57074a1029e22a
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2024-12-02 13:10:20 +0100

    mapa_hecho

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent _playerAgent;
+
+    void Awake()
     {
-        
+        _playerAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire2"))
+        {
+            SetDestinationPoint();
+        }
+    }
+
+    void SetDestinationPoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.layer == 3)
+            {
+                _playerAgent.destination = hit.point;
+                //_playerAgent.SetDestination(hit.point);
+            }
+        }
     }
 }
