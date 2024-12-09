bf13b0a6c911877376224324ae4d7ffc4a9dda54
RonixKuinn <154350005+RonixKuinn@users.noreply.github.com>
2024-12-09 12:38:00 +0100

    movidas raras

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,11 +14,19 @@ public class Enemy : MonoBehaviour
 
     public EnemyState currentState;
     private NavMeshAgent _AIAgent;
+    private Transform _playerTransform;
+
+    // ······ Cosas  ······//
     [SerializeField] Transform[] _patrolPoints;
+
+    // ······ Cosas detección ······//
+    [SerializeField] float _visionRange = 5;
+    [SerializeField] float _visionAngle = 120;
 
     void Awake()
     {
         _AIAgent = GetComponent<NavMeshAgent>();
+        _playerTransform = GameObject.FindWithTag("Player").transform;
     }
 
     void Start()
@@ -46,6 +54,11 @@ public class Enemy : MonoBehaviour
 
     void Patrol()
     { 
+        if(OnRange())
+        {
+            currentState = EnemyState.Chasing;
+        }
+
         if(_AIAgent.remainingDistance < 0.5f)
         {
           SetRandomPatrolPoint();  
@@ -54,12 +67,36 @@ public class Enemy : MonoBehaviour
 
     void Chase()
     {
+        if(!OnRange())
+        {
+            currentState = EnemyState.Patrolling;
+        }
 
+        _AIAgent.destination = _playerTransform.position;
     }
 
     void Search()
     {
 
+    }
+
+    bool OnRange()
+    {
+        Vector3 directionToPlayer = _playerTransform.position - transform.position;
+        float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+        float distanceToPlayer = Vector3.Distance(transform.position, _playerTransform.position);
+
+        if(distanceToPlayer > _visionRange)
+        {
+            return false;
+        }
+
+        if(angleToPlayer > _visionRange)
+        {
+            return false;
+        }
+
+        return true;
     }
 
     void SetRandomPatrolPoint()
@@ -74,5 +111,14 @@ public class Enemy : MonoBehaviour
             Gizmos.color = Color.blue;
             Gizmos.DrawWireSphere(point.position, 1);
         }
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _visionRange);
+
+        Gizmos.color = Color.yellow;
+        Vector3 fovLine1 = Quaternion.AngleAxis(_visionAngle * 0.5f, transform.up) * transform.forward * _visionRange;
+        Vector3 fovLine2 = Quaternion.AngleAxis(_visionAngle * 0.5f, transform.up) * transform.forward * _visionRange;
+        Gizmos.DrawLine(transform.position, transform.position + fovLine1);
+        Gizmos.DrawLine(transform.position, transform.position + fovLine2);
     }
 }
